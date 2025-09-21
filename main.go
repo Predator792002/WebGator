1342338a27fced97d333af0005caaba0cb9eaa69
Ayush Gupta <90502624+Predator792002@users.noreply.github.com>
2025-09-21 23:14:55 +0530

    B: Added command features and initialized postgres

--- main.go
+++ main.go
@@ -2,26 +2,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/Predator792002/WebGator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(cfg)
-
-	if err := cfg.SetUser("Ayush"); err != nil {
-		log.Fatal(err)
+		log.Fatalf("error reading config: %v", err)
 	}
 
-	cfg2, err := config.Read()
+	programState := &state{
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(cfg2)
 }
